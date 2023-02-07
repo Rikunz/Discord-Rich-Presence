2799b487a4cc1f3852d41867c45508d5c4c54dfd
Rikunz <thoriqfirdaus8@gmail.com>
2023-02-07 14:54:40 +0700

    Update for Presence sense

--- src/presence.js
+++ src/presence.js
@@ -2,162 +2,94 @@ module.exports = function pre(client, body) {
 
     if (body.action === "set") {
 
+        if (body.details === "www.youtube.com") {
+            if (body.smallText === "https://www.youtube.com/") {
+                body.state = "Viewing Home Page";
+                client.setActivity({
 
-        if (body.details === "daring.uin-suka.ac.id") {
+                    state: body.state,
+                    details: body.largeText,
+                    largeImageKey: "ytdark",
+                    largeImageText: body.largeText || body.title,
+                    smallImageKey: 'chrome',
+                    smallImageText: body.smallText,
+                    instance: true,
+                    type: 2,
+                }).then(console.log, console.error);
+            } else {
+                client.setActivity({
+
+                    state: body.state,
+                    details: body.largeText,
+                    startTimestamp: new Date(),
+                    largeImageKey: "ytdark",
+                    largeImageText: body.largeText || body.title,
+                    smallImageKey: 'chrome',
+                    smallImageText: body.smallText,
+                    instance: true,
+                    type: 2,
+                    buttons: [{ label: "View", url: body.smallText }]
+                }).then(console.log, console.error);
+            }
 
 
-            client.setActivity({
-
-                state: "Online",
-                details: "Belajar",
-                startTimestamp: new Date(),
-                largeImageKey: "belajar",
-                largeImageText: "Belajar",
-                smallImageKey: 'chrome',
-                smallImageText: "Chrome",
-                instance: true
-
-            }).then(console.log, console.error);
         }
 
         else
 
-            if (body.details === "akademik.uin-suka.ac.id") {
 
+            if (body.details === "music.youtube.com") {
 
                 client.setActivity({
 
-
-
-
-
-                    state: "Online",
-                    details: "Belajar",
+                    state: body.state,
+                    details: body.largeText,
                     startTimestamp: new Date(),
-                    largeImageKey: "belajar",
-                    largeImageText: "Belajar",
+                    largeImageKey: "ytdark",
+                    largeImageText: body.largeText || body.title,
                     smallImageKey: 'chrome',
-                    smallImageText: "Chrome",
-                    instance: true
+                    smallImageText: body.smallText,
+                    instance: true,
+                    type: 2,
+                    buttons: [{ label: "View", url: body.smallText }]
 
                 }).then(console.log, console.error);
+
             }
 
             else
 
-                if (body.details === "www.zenius.net") {
-
+                if (body.details === "github.com") {
 
                     client.setActivity({
 
-
-
-
-
-                        state: "Online",
-                        details: "Belajar",
+                        state: body.state,
+                        details: body.details,
                         startTimestamp: new Date(),
-                        largeImageKey: "belajar",
-                        largeImageText: "Belajar",
+                        largeImageKey: "github",
+                        largeImageText: body.largeText || body.title,
                         smallImageKey: 'chrome',
-                        smallImageText: "Chrome",
+                        smallImageText: body.smallText.includes("search") && body.smallText.length >= 128 ? body.title : body.smallText,
+                        instance: true,
+                        buttons: [{ label: "Repository", url: body.smallText }]
+
+                    }).then(console.log, console.error);
+
+                } else {
+
+                    client.setActivity({
+
+                        state: body.state,
+                        details: body.details,
+                        startTimestamp: new Date(),
+                        largeImageKey: "chrome",
+                        largeImageText: body.largeText || body.title,
+                        smallImageKey: 'chromeium',
+                        smallImageText: body.smallText.includes("search") && body.smallText.length >= 128 ? body.title : body.smallText,
                         instance: true
 
                     }).then(console.log, console.error);
                 }
-
-
-                else
-
-
-                    if (body.details === "www.youtube.com") {
-                        if (body.smallText === "https://www.youtube.com/") {
-                            body.state = "Viewing Home Page";
-                            client.setActivity({
-
-                                state: body.state,
-                                details: body.largeText,
-                                largeImageKey: "ytdark",
-                                largeImageText: body.largeText || body.title,
-                                smallImageKey: 'chrome',
-                                smallImageText: body.smallText,
-                                instance: true,
-                                type: 2,
-                            }).then(console.log, console.error);
-                        } else {
-                            client.setActivity({
-
-                                state: body.state,
-                                details: body.largeText,
-                                startTimestamp: new Date(),
-                                largeImageKey: "ytdark",
-                                largeImageText: body.largeText || body.title,
-                                smallImageKey: 'chrome',
-                                smallImageText: body.smallText,
-                                instance: true,
-                                type: 2,
-                                buttons: [{ label: "View", url: body.smallText }]
-                            }).then(console.log, console.error);
-                        }
-
-
-                    }
-
-                    else
-
-
-                        if (body.details === "music.youtube.com") {
-
-                            client.setActivity({
-
-                                state: body.state,
-                                details: body.largeText,
-                                startTimestamp: new Date(),
-                                largeImageKey: "ytdark",
-                                largeImageText: body.largeText || body.title,
-                                smallImageKey: 'chrome',
-                                smallImageText: body.smallText,
-                                instance: true,
-                                type: 2,
-                                buttons: [{ label: "View", url: body.smallText }]
-
-                            }).then(console.log, console.error);
-
-                        }
-
-                        else
-
-                            if (body.details === "github.com") {
-
-                                client.setActivity({
-
-                                    state: body.state,
-                                    details: body.details,
-                                    startTimestamp: new Date(),
-                                    largeImageKey: "github",
-                                    largeImageText: body.largeText || body.title,
-                                    smallImageKey: 'chrome',
-                                    smallImageText: body.smallText.includes("search") && body.smallText.length >= 128 ? body.title : body.smallText,
-                                    instance: true,
-                                    buttons: [{ label: "Repository", url: body.smallText }]
-
-                                }).then(console.log, console.error);
-
-                            } else {
-
-                                client.setActivity({
-
-                                    state: body.state,
-                                    details: body.details,
-                                    startTimestamp: new Date(),
-                                    largeImageKey: "chrome",
-                                    largeImageText: body.largeText || body.title,
-                                    smallImageKey: 'chromeium',
-                                    smallImageText: body.smallText.includes("search") && body.smallText.length >= 128 ? body.title : body.smallText,
-                                    instance: true
-
-                                }).then(console.log, console.error);
-                            }
 
 
 
